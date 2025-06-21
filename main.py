a5ad546e9e8ac4bd372a6a0d70557ded6a724799
andres <andres.carrillo@appliedmedical.com>
2025-06-21 11:19:25 -0700

    adding video and live feed options.

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from custom_widgets.camera_widget import CameraWidget
 from custom_widgets.output_widget import OutputWidget
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget,QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget,QHBoxLayout,QAction,QFileDialog
+from custom_widgets.camera_feed_editor import CameraFeedDialog
+import sys
+
+
+## TODO: Connect self._camera_source to the camera widget so that it can be updated when the user selects a new camera feed
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Camera and Output Widget")
+
         
+        file_menu = self.menuBar().addMenu("File")
+        edit_menu = self.menuBar().addMenu("Edit")
+
+        self.load_video_action = QAction("Load Video", self)
+        self.set_live_camera_action = QAction("Change Live Source", self)
+
+        self.change_upscale_model_action = QAction("Change Upscale Model", self)
+
+        file_menu.addAction(self.load_video_action)
+        file_menu.addAction(self.set_live_camera_action)
+
+        edit_menu.addAction(self.change_upscale_model_action)
         self.camera_widget = CameraWidget()
         self.output_widget = OutputWidget(self)
         
@@ -20,8 +38,37 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(container)
 
 
+        self.load_video_action.triggered.connect(self.update_video_source)
+        self.set_live_camera_action.triggered.connect(self.update_camera_source)
+        self.change_upscale_model_action.triggered.connect(self.update_upscale_model)
+
+        self._camera_source = -1  # Default value for camera source
+
+
+    def update_video_source(self):
+        # create file dialog to select video file
+        video_file, _ = QFileDialog.getOpenFileName(self, "Select Video File", "", "Video Files (*.mp4 *.avi *.mov)")
+        if video_file:
+            self.camera_widget.set_video_source(video_file)
+
+    def update_camera_source(self):
+        # create widget that displays available cameras
+        camera_feed_dialog = CameraFeedDialog(self)
+        camera_feed_dialog.exec_()
+        selected_camera = camera_feed_dialog.feed_option_widget.camera_index
+        # if a camera is selected, set the camera source in the camera widget
+        if selected_camera != -1:
+            if selected_camera != self._camera_source:
+                self._camera_source = selected_camera
+
+
+
+    def update_upscale_model(self):
+        pass
+        
+
 if __name__ == "__main__":
-    import sys
+
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
