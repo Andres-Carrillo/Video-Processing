654f63e0710a95e275e3d3f93680832f0e696b68
andres <andres.carrillo@appliedmedical.com>
2025-06-24 12:50:54 -0700

    debuging and ensuring input image is expected size

--- custom_workers/upscale_worker.py
+++ custom_workers/upscale_worker.py
@@ -1,11 +1,18 @@
 from custom_workers.base_worker import BaseImageWorker
 import onnx
 import onnxruntime as ort
+print("ONNX Runtime version:", ort.__version__)
+print("ort available providers:", ort.get_available_providers())
 from utils import qimage_to_cv_image,cv_image_to_qimage
 import numpy as np
 import cv2 as cv    
+import time
 
 class UpscaleWorker(BaseImageWorker):
+    input_size = (240,416)
+    video_writer = cv.VideoWriter("upscale_worker_output.mp4", cv.VideoWriter_fourcc(*'mp4v'), 30, (1664,960),isColor=False)  # Adjust the output size as needed
+    
+    frame_count = 0
     def __init__(self, model_type,is_deep_learning=True):
         super().__init__()
         
@@ -31,26 +38,48 @@ class UpscaleWorker(BaseImageWorker):
             # so that it can be emitted in the process method inhereted from BaseImageWorker  
             if self.ort_session is None:
                 self.start_session()
+
+
             
+           
+
             self.data = self.feed_model(data)
+            self.frame_count += 1
+
+            if self.frame_count > 300 == 0:  # Save every 30 frames
+                self.video_writer.release()
+                self.video_writer = cv.VideoWriter("upscale_worker_output" + str(self.frame_count) + ".mp4", cv.VideoWriter_fourcc(*'mp4v'), 30, (960, 1664)) 
+                print(f"Saving frame {self.frame_count} to video file.")
+            
         
+
+    def _resize_input(self, image):
+        image = cv.resize(image, (self.input_size[1], self.input_size[0]), interpolation=cv.INTER_CUBIC)
+        return image
+
     def _init_onnx_runtime(self, model_type):
         self.model_path = None
         self.ort_session = None
         provider = ort.get_available_providers()
+        print("Available ONNX Runtime providers: ", provider)
         self.device = "CUDAExecutionProvider" if "CUDAExecutionProvider" in provider else "CPUExecutionProvider"
 
         if model_type == "esrgan":
             self.model_path = "model_zoo/model.onnx"
 
     def start_session(self):
+        print("starting onnx runtime session using : ",self.device)
         if self.ort_session is None:
             self.ort_session = ort.InferenceSession(self.model_path, providers=[self.device])
+
 
         return self.ort_session
     
     def feed_model(self, data):
         image = qimage_to_cv_image(data)
+        source_shape = image.shape
+        if source_shape[0] != self.input_size[1] or source_shape[1] != self.input_size[0]:
+                image = self._resize_input(image)
 
         image_np = image.astype(np.float32)
         image_np = image_np.transpose(2, 0, 1)
@@ -58,12 +87,34 @@ class UpscaleWorker(BaseImageWorker):
         image_tensor = image_np[None,...]
 
         model_input = {self.ort_session.get_inputs()[0].name: image_tensor}
-        model_output = self.ort_session.run(None,model_input)
 
-        output_image = model_output[0]
-        output_image = output_image.squeeze(0).transpose(1, 2, 0)
+        start = time.perf_counter()
+
+        try:
+            model_output = self.ort_session.run(None,model_input)
+            end = time.perf_counter()
+            print(f"Model inference time: {end - start:.4f} seconds")
+            output_image = model_output[0]
+
+            print("Output shape: ", output_image.shape)
+            output_image = output_image.squeeze(0).transpose(1, 2, 0)
+
+            output_image = cv.cvtColor(output_image,cv.COLOR_RGB2GRAY)
+            cv.imwrite("output/output_image_" +str(self.frame_count)+".png", output_image)
+            self.video_writer.write(output_image.astype(np.uint8))
+
+            print("Output shape after squeeze and transpose: ", output_image.shape)
+        except Exception as e:
+            print(f"Error during model inference: {e}")
+            output_image = np.zeros((self.input_size[0], self.input_size[1]), dtype=np.uint8)
+            self.video_writer.write(output_image)
+            self.video_writer.release()    
 
         return cv_image_to_qimage(output_image.astype(np.uint8))
         
     
+    def stop_thread(self):
+        self.data = None
+        self.video_writer.release()
         
+        self.wait()
